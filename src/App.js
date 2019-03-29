6a8b184bae3333ff7d1f067bdaaa559f2e5c375c
David Bolin <david@bolin.at>
2019-03-29 10:12:39 -0700

    basic html for title and searchbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <main className='App'>
+        <h1>Star Wars Search</h1>
+        <form>
+          <label htmlFor="searchBox">Name:</label>
+          <input id="searchBox" className="searchTerm"></input>
+        </form>
+      </main>
+    );
+  }
 }
-
+  
 export default App;
