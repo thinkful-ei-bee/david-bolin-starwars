8c4eaaceb51332f69bc9482684fb38ef5e3a15c3
David Bolin <david@bolin.at>
2019-03-29 10:18:58 -0700

    made input controlled state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
   
+  state = {
+    searchTerm: ''
+  }
+
+  updateInputBox = (str) => {
+    this.setState({
+      searchTerm: str
+    });
+  }
+
   render() {
     return (
       <main className='App'>
         <h1>Star Wars Search</h1>
         <form>
           <label htmlFor="searchBox">Name:</label>
-          <input id="searchBox" className="searchTerm"></input>
+          <input type="text" id="searchBox" className="searchTerm" value={this.state.searchTerm} onChange={e => this.updateInputBox(e.target.value)}/>
         </form>
       </main>
     );
